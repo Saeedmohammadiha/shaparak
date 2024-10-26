bc88ece7dcf05dbc465dc04301e36f9cf752ba2e
saeed mohammdiha <saeed1440124@gmail.com>
2024-10-26 16:03:31 +0330

    feat: add the origin cards component

---
+++ src/ui/components/features/CardToCardFrom/OriginCardsSelect.tsx
@@ -0,0 +1,39 @@
+import { MenuItem } from '@mui/material';
+import { useGetCardsList } from 'business/hooks/shaparak/getCardsList/useGetCardsList';
+import { LoadingContext } from 'business/stores/shaparak/loadingContext';
+import { useContext, useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { ControlledSelect } from 'ui/htsc-components/ControlledInput/ControlledSelect';
+
+export default function OriginCardsSelect() {
+	const { t } = useTranslation();
+	const { control } = useFormContext();
+	const { data, isLoading } = useGetCardsList();
+	const { setLoading } = useContext(LoadingContext);
+
+	useEffect(() => {
+		setLoading(isLoading);
+	}, [isLoading]);
+
+	//TODO: get the list of cards and render inside the select
+	return (
+		<ControlledSelect
+			control={control}
+			name="origin"
+			label={t('originCardNumber')}
+			onChange={() => {}}
+		>
+			{data?.map((card) => {
+				return (
+					<MenuItem
+						key={card.cardIndex}
+						value={card.cardIndex}
+					>
+						{card.maskedPan}
+					</MenuItem>
+				);
+			})}
+		</ControlledSelect>
+	);
+}
