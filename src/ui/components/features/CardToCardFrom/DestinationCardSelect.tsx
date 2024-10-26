5f340e8f7d35fd2a739cd1c50b7e2510d9c8e991
saeed mohammdiha <saeed1440124@gmail.com>
2024-10-26 16:04:08 +0330

    feat: add the destination cards component

---
+++ src/ui/components/features/CardToCardFrom/DestinationCardSelect.tsx
@@ -0,0 +1,41 @@
+import { MenuItem } from '@mui/material';
+import { useGetDestinationCards } from 'business/hooks/shaparak/getDestinationCards/useGetDestinationCards';
+import { LoadingContext } from 'business/stores/shaparak/loadingContext';
+import { useContext, useEffect } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import { ControlledSelect } from 'ui/htsc-components/ControlledInput/ControlledSelect';
+
+export default function DestinationCardSelect() {
+	const { t } = useTranslation();
+	const { control } = useFormContext();
+	const { setLoading } = useContext(LoadingContext);
+	const { data, isLoading } = useGetDestinationCards();
+
+	useEffect(() => {
+		setLoading(isLoading);
+	}, [isLoading]);
+
+	//TODO: // call the dadehaye por karbord and show to user
+
+	return (
+		<ControlledSelect
+			control={control}
+			name="destination"
+			label={t('destinationCardNumber')}
+			onChange={() => {}}
+		>
+			{data?.datas.map((card) => {
+				return (
+					<MenuItem
+						key={card.value}
+						value={card.value}
+					>
+						{card.value}
+						{card.title}
+					</MenuItem>
+				);
+			})}
+		</ControlledSelect>
+	);
+}
