cabfcae0b4f6cc3737872339946f2490b34d5a02
saeed mohammdiha <saeed1440124@gmail.com>
2024-10-28 11:04:07 +0330

    refactor: update the register call to open the shapark site

--- src/business/hooks/api-calls/register/useRegister.ts
+++ src/business/hooks/api-calls/register/useRegister.ts
@@ -1,23 +1,46 @@
 import { Mediator } from '@Mediatr/index';
 import { useMutation } from '@tanstack/react-query';
 import { RegisterCommand } from 'business/application/shaparak/register/command';
+import { pushAlert } from 'business/stores/AppAlertsStore';
 
 import { ErrorType } from 'common/entities/ErrorType';
 import { RegisterResponse } from 'common/entities/http-dtos/register/response';
+import { Dispatch, SetStateAction } from 'react';
 
 const mediator = new Mediator();
 
-export function useRegister() {
-	return useMutation<RegisterResponse, ErrorType<object>, RegisterCommand>({
+type Props = {
+	open?: boolean;
+	clearAlerts: () => void;
+	setOpen: Dispatch<SetStateAction<boolean>>;
+};
+
+export function useRegister({ setOpen, clearAlerts }: Props) {
+	const closeModal = () => {
+		setOpen(false);
+		clearAlerts();
+	};
+
+	return useMutation<RegisterResponse, ErrorType<object>>({
 		mutationFn: () => mediator.send<RegisterResponse>(new RegisterCommand()),
+		mutationKey: ['register'],
+		cacheTime: 10 * 60 * 1000,
 		onMutate: (variables) => {
 			return () => variables;
 		},
-		onSuccess: (data) => {
-			return () => data;
+		onSuccess: (res) => {
+			closeModal();
+			const url = res.registrationAddress.replace('tsm.shaparak.ir', '10.42.7.39');
+			window.open(url, '_blank');
 		},
-		onError: (_, variables) => {
-			return () => variables;
+		onError: (err) => {
+			console.log(err);
+			closeModal();
+			pushAlert({
+				type: 'error',
+				messageText: err.detail,
+				hasConfirmAction: true
+			});
 		}
 	});
 }
