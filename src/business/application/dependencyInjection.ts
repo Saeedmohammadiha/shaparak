74d7ff09edaca0a28cfd4125612647764d14c117
saeed mohammdiha <saeed1440124@gmail.com>
2024-10-28 11:00:34 +0330

    refactor: update the dependency injection and endpoint file with new apis

--- src/business/application/dependencyInjection.ts
+++ src/business/application/dependencyInjection.ts
@@ -6,3 +6,15 @@ export * from './shaparak/get-cards-list/query';
 
 export * from './shaparak/get-destination-cards/handler';
 export * from './shaparak/get-destination-cards/Query';
+
+export * from './shaparak/register/command';
+export * from './shaparak/register/handler';
+
+export * from './shaparak/get-key/command';
+export * from './shaparak/get-key/handler';
+
+export * from './shaparak/confirm-registeration-card/command';
+export * from './shaparak/confirm-registeration-card/handler';
+
+export * from './shaparak/inquiry-destination-holder/command';
+export * from './shaparak/inquiry-destination-holder/handler';
--- src/business/infrastructure/end-points.ts
+++ src/business/infrastructure/end-points.ts
@@ -1,3 +1,6 @@
 export const GET_CARDS_LIST = '/CardOperations/Shaparak/getcardslist';
 export const GET_DESTINATION_CARDS = '/CustomerManager/FrequentlyUsedData/GetSources';
 export const REGISTER = '/CardOperations/Shaparak/registercard';
+export const GET_KEY = '/mobileApp/getKey';
+export const CONFIRM_REGISTRATION_CARD = '/CardOperations/Shaparak/confirmregistrationcard';
+export const INQUIRY_DESTINATION_HOLDER = '/CardOperations/Shaparak/inquirydestinationholder';
