247a2723b09f61c9c28a0e23f2d78763d6857ac8
saeed mohammdiha <saeed1440124@gmail.com>
2024-10-29 08:59:23 +0330

    refactor: add the layout to the router

--- src/ui/route-config/paths.ts
+++ src/ui/route-config/paths.ts
@@ -1,3 +1,6 @@
 export const paths = {
-	Home: '/shaparak'
+	Home: '/',
+	layout: '',
+	Overview: 'overview',
+	Receipt: 'receipt'
 };
--- src/ui/route-config/routes.tsx
+++ src/ui/route-config/routes.tsx
@@ -1,17 +1,33 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import PageLayout from 'ui/components/layout/PageLayout';
 import ErrorPage from 'ui/pages/ErrorPage';
 import Home from 'ui/pages/Home';
+import Overview from 'ui/pages/Overview';
 import Layout from '../pages/Layout';
 import { paths } from './paths';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <Layout />,
+			errorElement: <ErrorPage />,
+			children: [
+				{
+					path: paths.layout,
+					element: <PageLayout />,
+					children: [
+						{ index: true, path: paths.Home, element: <Home /> },
+						{ path: paths.Overview, element: <Overview /> }
+					]
+				}
+			]
+		}
+	],
 	{
-		path: '/',
-		element: <Layout />,
-		errorElement: <ErrorPage />,
-		children: [{ index: true, path: paths.Home, element: <Home /> }]
+		basename: import.meta.env.BASE_URL
 	}
-]);
+);
 
 export default router;
