e8bc65a541ca9160d2d541062a5a44658703288e
saeed mohammdiha <saeed1440124@gmail.com>
2024-10-29 09:13:24 +0330

    refactor: add errors and chnage loading in cardslist comp

--- src/ui/components/features/CardToCardFrom/OriginCardsSelect.tsx
+++ src/ui/components/features/CardToCardFrom/OriginCardsSelect.tsx
@@ -1,28 +1,26 @@
 import { MenuItem } from '@mui/material';
 import { useGetCardsList } from 'business/hooks/api-calls/get-cards-list/useGetCardsList';
-import { LoadingContext } from 'business/stores/shaparak/loadingContext';
-import { useContext, useEffect } from 'react';
+import { useLoadingHandler } from 'business/stores/shaparak/loadingStore';
+import { InquiryDestinationCommand } from 'main';
 import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { ControlledSelect } from 'ui/htsc-components/ControlledInput/ControlledSelect';
 
 export default function OriginCardsSelect() {
 	const { t } = useTranslation();
-	const { control } = useFormContext();
+	const { control, formState } = useFormContext<InquiryDestinationCommand>();
 	const { data, isLoading } = useGetCardsList();
-	const { setLoading } = useContext(LoadingContext);
-
-	useEffect(() => {
-		setLoading(isLoading);
-	}, [isLoading]);
+	useLoadingHandler(isLoading);
 
 	//TODO: get the list of cards and render inside the select
 	return (
 		<ControlledSelect
 			control={control}
-			name="origin"
+			name="cardIndex"
 			label={t('originCardNumber')}
 			onChange={() => {}}
+			error={!!formState.errors.cardIndex?.message}
+			helperText={formState.errors.cardIndex?.message}
 		>
 			{data?.map((card) => {
 				return (
