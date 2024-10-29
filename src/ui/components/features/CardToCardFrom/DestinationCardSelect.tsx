d1513ba28047170dfcda78b1d535c29d22991ebb
saeed mohammdiha <saeed1440124@gmail.com>
2024-10-29 09:09:30 +0330

    refactor: change the loading and add errors to destinatin cards select

--- src/ui/components/features/CardToCardFrom/DestinationCardSelect.tsx
+++ src/ui/components/features/CardToCardFrom/DestinationCardSelect.tsx
@@ -1,29 +1,27 @@
 import { MenuItem } from '@mui/material';
 import { useGetDestinationCards } from 'business/hooks/api-calls/get-destination-cards/useGetDestinationCards';
-import { LoadingContext } from 'business/stores/shaparak/loadingContext';
-import { useContext, useEffect } from 'react';
+import { useLoadingHandler } from 'business/stores/shaparak/loadingStore';
+import { InquiryDestinationCommand } from 'main';
 import { useFormContext } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { ControlledSelect } from 'ui/htsc-components/ControlledInput/ControlledSelect';
 
 export default function DestinationCardSelect() {
 	const { t } = useTranslation();
-	const { control } = useFormContext();
-	const { setLoading } = useContext(LoadingContext);
+	const { control, formState } = useFormContext<InquiryDestinationCommand>();
 	const { data, isLoading } = useGetDestinationCards();
-
-	useEffect(() => {
-		setLoading(isLoading);
-	}, [isLoading]);
+	useLoadingHandler(isLoading);
 
 	//TODO: // call the dadehaye por karbord and show to user
 
 	return (
 		<ControlledSelect
 			control={control}
-			name="destination"
+			name="destinationCardNo"
 			label={t('destinationCardNumber')}
 			onChange={() => {}}
+			error={!!formState.errors.destinationCardNo?.message}
+			helperText={formState.errors.destinationCardNo?.message}
 		>
 			{data?.datas.map((card) => {
 				return (
